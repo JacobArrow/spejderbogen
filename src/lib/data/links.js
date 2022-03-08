cddd42799318daed34dfb70ef7a455b56f792338
JacobArrow <jacobengb@gmail.com>
2022-03-09 00:17:03 +0100

    Added icon to drawer link

--- src/lib/data/links.js
+++ src/lib/data/links.js
@@ -5,6 +5,7 @@ import PencilIcon from 'svelte-material-icons/LeadPencil.svelte';
 import PlaylistIcon from 'svelte-material-icons/PlaylistMusic.svelte';
 import FormatListIcon from 'svelte-material-icons/FormatListBulletedSquare.svelte';
 import InfoIcon from 'svelte-material-icons/InformationOutline.svelte';
+import HeartIcon from 'svelte-material-icons/Heart.svelte';
 
 export const NAVBAR_LINKS = [
 	{ name: 'Forside', link: '/' },
@@ -18,6 +19,6 @@ export const DRAWER_LINKS = [
 	{ name: 'Alle sange', link: '/sange', icon: PlaylistIcon },
 	{ name: 'Kategorier', link: '/kategorier', icon: FormatListIcon },
 	{ name: 'Forfattere', link: '/forfattere', icon: PencilIcon },
-	{ name: 'Favoritter', link: '/favoritter' },
+	{ name: 'Favoritter', link: '/favoritter', icon: HeartIcon },
 	{ name: 'Om Spejderbogen', link: '/om-spejderbogen', icon: InfoIcon }
 ];
