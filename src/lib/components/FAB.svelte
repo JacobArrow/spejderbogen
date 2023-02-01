7ed3d4038283f7392aa07bf66a4cc8c0e3e0d1be
JacobArrow <jacobengb@gmail.com>
2023-02-02 00:01:39 +0100

    BUG-71: Added mobile margin

--- src/lib/components/FAB.svelte
+++ src/lib/components/FAB.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="fixed z-50 right-0 md:right-8 2xl:right-14 mb-6 md:mb-8 2xl:mb-10 w-full md:w-min"
+	class="fixed z-50 right-0 md:right-8 2xl:right-14 mb-4 sm:mb-6 md:mb-8 2xl:mb-10 w-full md:w-min"
 	style="bottom:{$scrollPosition.footerOffset}px"
 >
 	<slot />
